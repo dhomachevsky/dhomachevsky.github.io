03c3f0b722a0837c3580a4127476f7139de81e2e
dhomachevsky <dhomachevsky@gmail.com>
2026-07-13 19:11:12 +0300

    upload all files

---
+++ js/main.js
@@ -0,0 +1,9 @@
+import { initGallery } from "./components/gallery.js";
+import { initLightbox } from "./components/lightbox.js";
+import { initTabs } from "./components/tabs.js";
+import { initResume } from "./components/resume.js";
+
+initGallery();
+initLightbox();
+initTabs();
+initResume();
