4008971da22c55b84ca7c24c4ee8989104ef40ff
dhomachevsky <dhomachevsky@gmail.com>
2026-07-13 19:10:43 +0300

    delete all files

--- js/main.js
+++
@@ -1,7 +0,0 @@
-import { initGallery } from "./components/gallery.js";
-import { initLightbox } from "./components/lightbox.js";
-import { initTabs } from "./components/tabs.js";
-
-initGallery();
-initLightbox();
-initTabs();
